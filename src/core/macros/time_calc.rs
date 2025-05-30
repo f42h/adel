797f40f69d31e29054f3a375f73365999ea5f3a6
f42h <felix.hansow@tuta.com>
2025-05-30 09:52:47 +0200

    Update comments

--- src/core/macros/time_calc.rs
+++ src/core/macros/time_calc.rs
@@ -1,5 +1,6 @@
-// (self.configs.delay_hour * 3600) + (self.configs.delay_min * 60) + self.configs.delay_sec
-
+// Specified by "DELETE_AFTER_N" setting:
+// Calculate how long to wait before clearing 
+// stored files and directories in ~/.adel
 #[macro_export]
 macro_rules! deletedelay {
     ($n:expr) => {
@@ -7,8 +8,11 @@ macro_rules! deletedelay {
     };
 }
 
+// Specified by "DELAY_HOUR", "DELAY_MIN", "DELAY_SEC" settings:
+// Calculate how long to wait before starting a new scan in the 
+// directories specified by the "ADEL_DIRS" setting
 #[macro_export]
-macro_rules! scandelay { // Delete after 
+macro_rules! scandelay { 
     ($hour:expr, $min:expr, $sec:expr) => {
         ($hour * 3600) + ($min * 60) + $sec
     }
